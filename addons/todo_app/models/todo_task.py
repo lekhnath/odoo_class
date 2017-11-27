d74cc0d4637cf174a9ab7c8021b668bdc532c3d8
Lekhnath Rijal <049lekhnath@gmail.com>
2017-11-27 13:14:47 +0545

    orm api

--- addons/todo_app/models/todo_task.py
+++ addons/todo_app/models/todo_task.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 class TodoTask(models.Model):
   _name = 'todo_app.task'
@@ -7,8 +7,13 @@ class TodoTask(models.Model):
   """
   _rec_name = 'title'
 
+  active = fields.Boolean(
+    'Is Active?',
+    default=True
+  )
   title = fields.Char(
-    'Tilte/Name'
+    'Tilte/Name',
+    required=True
   )
   is_done = fields.Boolean(
     'Task Done?'
@@ -16,3 +21,24 @@ class TodoTask(models.Model):
   date_deadline = fields.Date(
     'Deadline'
   )
+
+  @api.model
+  def archive_done(self):
+    # completed_todos = self.search([('is_done', '=', True)])
+    # completed_todos.write({
+    #   'active': False
+    # })
+    for todo_task in self.search([('is_done', '=', True)]):
+      todo_task.active = False
+
+    return True
+
+  @api.multi
+  def toggle_done(self):
+    self.ensure_one() #singletone recordset
+
+    # Self is record set and follows active record set mechanism
+    for todo_task in self:
+      todo_task.is_done = not todo_task.is_done
+
+    return True
