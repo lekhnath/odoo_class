f3ad72bbb41f1c8bffeb1ade49883892b174c496
Lekhnath Rijal <049lekhnath@gmail.com>
2017-11-27 14:44:15 +0545

    table relation

--- addons/enhance_todo_app/models/models.py
+++ addons/enhance_todo_app/models/models.py
@@ -1,5 +1,11 @@
 from odoo import models, fields
 
+class TodoTag(models.Model):
+    _name='enhance_todo_app.tag'
+
+    name = fields.Char('Name')
+    active = fields.Boolean('Is Active?', default=True)
+
 class EnhanceTodoTask(models.Model):
     _inherit = 'todo_app.task'
 
@@ -10,3 +16,14 @@ class EnhanceTodoTask(models.Model):
     ],
     string = 'Task Priority',
     default='low')
+
+    assignee_id = fields.Many2one(
+        string='Assigned To',
+        comodel_name='res.users',
+        ondelete='set null',
+    )
+
+    tag_ids = fields.Many2many(
+        comodel_name='enhance_todo_app.tag',
+        string='Tags'
+    )
